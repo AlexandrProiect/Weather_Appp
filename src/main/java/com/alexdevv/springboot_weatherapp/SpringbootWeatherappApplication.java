0ec5557af237bd6e44c8472b71a6a39df2067ce6
Alexandr Dev <mak3d0nsk1@gmail.com>
2025-04-09 22:11:11 +0300

    schimbări

--- src/main/java/com/alexdevv/springboot_weatherapp/SpringbootWeatherappApplication.java
+++ src/main/java/com/alexdevv/springboot_weatherapp/SpringbootWeatherappApplication.java
@@ -2,8 +2,10 @@ package com.alexdevv.springboot_weatherapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.alexdevv.springboot_weatherapp")
 public class SpringbootWeatherappApplication {
 
 	public static void main(String[] args) {
